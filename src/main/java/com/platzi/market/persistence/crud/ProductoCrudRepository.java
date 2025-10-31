7065473234704d3f6d2f5e5e32a41f14aea8a741
DAVID RUSSELL GUTIERREZ ANGUIANO <p-dgutierrezan@izzi.mx>
2025-10-31 10:37:55 -0600

    Prueba commit desde IntelliJ

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -1,4 +1,14 @@
 package com.platzi.market.persistence.crud;
 
-public interface ProductoCrudRepository {
+import com.platzi.market.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstadi(int cantidadStock, boolean estado);
 }
