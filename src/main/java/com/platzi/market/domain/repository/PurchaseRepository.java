145560bcbd821fb317849972febce65e539dbfda
DAVID RUSSELL GUTIERREZ ANGUIANO <p-dgutierrezan@izzi.mx>
2025-11-04 17:28:06 -0600

    Mapeo

---
+++ src/main/java/com/platzi/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.platzi.market.domain.repository;
+
+import com.platzi.market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getall();
+    Optional<List<Purchase>> getByClient(int clientId);
+    Purchase save(Purchase purchase);
+}
